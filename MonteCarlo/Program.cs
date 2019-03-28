42b176d5b4f459a85852b28dfddb686291e7cbd5
Aquileo Reyes <aquileoreyes@gmail.com>
2019-03-28 10:24:52 -0400

    Code works

--- MonteCarlo/Program.cs
+++ MonteCarlo/Program.cs
@@ -6,82 +6,111 @@ using System.Threading.Tasks;
 
 namespace MonteCarlo
 {
-    public class Program
+public class Program
     {
-        /*
-            public double X, Y;
 
-            public Program(double X, double Y)
+        public static void Main(string[] args)
+        {
+            double withinn = 0.0;
+            Random varaible = new Random();
+
+            Console.WriteLine("How many points do you want the Monte Carlo method to run?");
+            Console.Write("\nInput answer: ");
+
+            int variable = Convert.ToInt32(Console.ReadLine());
+            MonterCarlo[] PossiblePoint = new MonterCarlo[variable];
+
+            for (int i = 0; i < variable; i++)
             {
-                this.X = X;
-                this.Y = Y;
-            }
 
-            public double Monte(int n)
-            {
-                Random random = new Random();
+                PossiblePoint[i] = new MonterCarlo(varaible);
 
-                double within = 0.0;
+                if (PossiblePoint[i].Monte(PossiblePoint[i]) <= 1)
 
-                for (double i = 0; i < n; i++)
                 {
-                    this.X = random.NextDouble();
-                    this.Y = random.NextDouble();
-
-                    if (Math.Pow(X, 2) + Math.Pow(Y, 2) <= 1)
-                    {
-                        within++;
-                    }
-
+                    withinn++;
                 }
-                return 4.0 * ((double)within / (double)n);
+
             }
+            double Answer = 4.0 * ((double)withinn / (double)variable);
+            double diffrence = Math.Abs(Math.PI - Answer);
 
-*/
+
+            Console.WriteLine("\nThe answers for different  tests are displayed below.\n");
+            Console.WriteLine($"Remember that true Pi equlas {Math.PI}.");
+            Console.WriteLine($"\nThe answers for your input is: {Answer} .\n");
+            Console.WriteLine($"The diffrence between True pi and your answer is {diffrence}.\n");
 
 
+            Console.WriteLine("\nBelow are result of predetermined numbers.\n");
 
-            
-            public static double Monte(int n)
+
+            int num = 1;
+            for (int n = 0; n < 6; n++)
             {
+                num *= 10;
 
 
-            double X = 0.0, Y = 0.0;
-            Random random = new Random();
-
-                double within = 0.0;
-
-                for (double i = 0; i < n; i++)
-                {
-                    X = random.NextDouble();
-                    Y = random.NextDouble();
-
-                    if (Math.Pow(X, 2) + Math.Pow(Y, 2) <= 1)
-                    {
-                        within++;
-                    }
-
-                } return 4.0 * ((double) within / (double) n);
-            }
-
-        
-
-            public static void Main()
-            {
-       
-
-            Console.WriteLine("\nThe answers for each test are displayed below.\n");
-
-                int num = 1;
-                for (int n = 0; n < 7; n++)
-                {
-                    num *= 10;
-
-                    Console.WriteLine($"Amount of times the code was ran {num} : Output of the code { Monte(num)}\n");
-                }
+                Console.WriteLine($"Amount of times the code was ran {num} : Output of the code {Monte(num)}\n");
             }
         }
+
+
+        public static double Monte(int n)
+
+        {
+            double X = 0.0, Y = 0.0;
+
+            Random random = new Random();
+
+            double within = 0.0;
+
+            for (double i = 0; i < n; i++)
+
+            {
+
+                X = random.NextDouble();
+
+                Y = random.NextDouble();
+
+
+
+                if (Math.Pow(X, 2) + Math.Pow(Y, 2) <= 1)
+
+                {
+
+                    within++;
+
+                }
+
+            }
+            return 4.0 * ((double)within / (double)n);
+
+        }
+        
     }
+}
+public struct MonterCarlo
+{
+    public double X, Y;
+
+    public MonterCarlo(double X, double Y)
+    {
+        this.X = X;
+        this.Y = Y;
+    }
+    public MonterCarlo(Random randomValue)
+    {
+        X = randomValue.NextDouble();
+        Y = randomValue.NextDouble();
+    }
+
+    public double Monte(MonterCarlo equation)
+    {
+        return Math.Sqrt(Math.Pow(equation.X, 2) + Math.Pow(equation.Y, 2));
+    }
+}
+
 
 
 
